cbb206c1000f2289c47197b4eb66475ed300a80c
ait-assou <0724015@cgodin.qc.ca>
2026-05-27 22:14:40 -0400

    ex 1 first commit

---
+++ ex1/js/changerStyle.js
@@ -0,0 +1,8 @@
+const paragraphe = document.getElementById("texte")
+
+function changerStyle() {    
+    console.log(paragraphe.textContent);
+    paragraphe.style.fontSize = "12px"
+    paragraphe.style.fontFamily = "Comic Sans MS "
+    paragraphe.style.color = "blue"
+}
