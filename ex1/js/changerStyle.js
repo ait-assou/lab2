cf363ea6cd396554bf33dd4970c779efe4eda85a
ait-assou <0724015@cgodin.qc.ca>
2026-05-27 22:32:33 -0400

    style btn

--- ex1/js/changerStyle.js
+++ ex1/js/changerStyle.js
@@ -1,4 +1,5 @@
 const paragraphe = document.getElementById("texte")
+const btnJSStyle = document.getElementById("jsStyle")
 
 function changerStyle() {    
     console.log(paragraphe.textContent);
@@ -6,3 +7,21 @@ function changerStyle() {
     paragraphe.style.fontFamily = "Comic Sans MS "
     paragraphe.style.color = "blue"
 }
+
+btnJSStyle.style.width = "10rem"
+btnJSStyle.style.backgroundColor = "#67CA62"
+btnJSStyle.style.border = "none"
+btnJSStyle.style.padding = "0.5rem 1rem"
+btnJSStyle.style.marginTop = "0.5rem"
+btnJSStyle.style.cursor = "pointer"
+
+btnJSStyle.onmouseenter = () => {
+    btnJSStyle.style.opacity = 0.8
+}
+
+btnJSStyle.onmouseleave = () => {
+    btnJSStyle.style.opacity = 1
+}
+
+
+
